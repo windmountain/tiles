ee44f3e9a0c43998e80a4540ea81e87d0058403b
Zach Rose <zach@windmountainsoftware.com>
2025-01-21 20:37:38 -0500

    Simplify adjacenct tile detection

--- src/core.ts
+++ src/core.ts
@@ -1,26 +1,26 @@
 export const newGame = () => {
-  const array = [0,1,2,3,4,5,6,7];
+  const array = [0, 1, 2, 3, 4, 5, 6, 7];
   for (var i = array.length - 1; i >= 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
   return [...array, 8];
 }
 
-const isAdjacentTo8 = (n, tiles) => {
+export const isAdjacentTo8 = (n, tiles) => {
   const indexOf8 = tiles.indexOf(8);
   const indexOfN = tiles.indexOf(n);
-  const xs = [0,1,2,0,1,2,0,1,2];
-  const ys = [0,0,0,1,1,1,2,2,2];
+  const xs = [0, 1, 2, 0, 1, 2, 0, 1, 2];
+  const ys = [0, 0, 0, 1, 1, 1, 2, 2, 2];
   const xOf8 = xs[indexOf8];
   const yOf8 = ys[indexOf8];
   const xOfN = xs[indexOfN];
   const yOfN = ys[indexOfN];
   const deltaX = Math.abs(xOf8 - xOfN);
   const deltaY = Math.abs(yOf8 - yOfN);
-  return deltaX === 1 && deltaY === 0 || deltaX === 0 && deltaY == 1;
+  return deltaX + deltaY === 1;
 }
 
 const swap = (n, tiles) => {
@@ -34,4 +34,8 @@ const swap = (n, tiles) => {
 
 export const move = (n, tiles) => {
   return isAdjacentTo8(n, tiles) ? swap(n, tiles) : [...tiles];
+}
+
+export const isSolved = (tiles) => {
+  return tiles.reduce((memo, tile, index) => memo && tile === index, true);
 }

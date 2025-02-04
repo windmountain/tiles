bcb4e6ad1d055fbb042d7c4e85682a7403cb4f2a
Zach Rose <zach@windmountainsoftware.com>
2025-02-04 12:01:01 -0500

    Connect to Fireproof Cloud

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useDocument, useLiveQuery } from 'use-fireproof'
+import { rawConnect } from "@fireproof/cloud";
+import { useFireproof } from 'use-fireproof'
 import { newGame, move, isAdjacentTo8, isSolved } from './core'
 import './App.css'
 
 
 function App() {
-  const response = useLiveQuery('date', {limit: 1, descending: true})
+  const { database, useLiveQuery, useDocument } = useFireproof("tiles2");
+  rawConnect(database, "zachs-tiles")
+  const response = useLiveQuery('date', { limit: 1, descending: true })
   const games = response.docs
   const [game, setGame, saveGame] = useDocument(() => ({
     tiles: newGame(),
@@ -14,34 +17,35 @@ function App() {
 
   const makeMove = (n, game) => {
     const newTiles = move(n, game.tiles);
-    useLiveQuery.database.put({
+    database.put({
       ...game,
       tiles: newTiles
     })
   }
 
   return (
-    <>
+    <div className="games">
+
       {games.map((game) => (
-        <>
-        { isSolved(game.tiles) && <h1>Solved!</h1> }
-        <div key={game._id} id="tileGame">
-          {game.tiles.map((n) =>
-            <button
-              key={n+1}
-              id={`tile-${n+1}`}
-              disabled={n == 8 || !isAdjacentTo8(n, game.tiles) || isSolved(game.tiles)}
-              onClick={(e) => makeMove(n, game)}
+        <div className="game" id={game._id}>
+          {isSolved(game.tiles) && <h1>Solved!</h1>}
+          <div key={game._id} id="tileGame">
+            {game.tiles.map((n) =>
+              <button
+                key={n + 1}
+                id={`tile-${n + 1}`}
+                disabled={n == 8 || !isAdjacentTo8(n, game.tiles) || isSolved(game.tiles)}
+                onClick={(e) => makeMove(n, game)}
               >
-            </button>
-          )}
+              </button>
+            )}
+          </div>
         </div>
-        </>
       ))}
       <button id="newGame" onClick={async () => { await saveGame() }}>
         New Game
       </button>
-    </>
+    </div>
   )
 }
 
